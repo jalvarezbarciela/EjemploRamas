bb7ef770e0599431d0f8aba121190c1796e74566
jalvarezbarciela <jalvarezbarciela@danielcastelao.org>
2020-01-10 14:16:58 +0100

    Rojo

--- src/ejemploramas/EjemploRamas.java
+++ src/ejemploramas/EjemploRamas.java
@@ -15,7 +15,7 @@ public class EjemploRamas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola Rojo");
     }
     
 }
